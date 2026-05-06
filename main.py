0ed40947b4a9860fe00ede3853fc6dd8487e12ba
daniel <daniel.j.sterzel@gmaik.com>
2026-05-06 12:56:59 +0200

    Loading data into pd dataframes and sequence config model

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from dataclasses import dataclass
 from pathlib import Path
 
+import cv2
+import pandas as pd
+import matplotlib.pyplot as plt
 
-class Config:
-    dataset_root: Path
-    sequence_name: str
-
-config = Config(
-    dataset_root=Path("./evs_mot_public_dataset/evs_mot-train"),
-    sequence_name="MOT_02"
-)
---
+++ sequence_config.py
@@ -0,0 +1,62 @@
+import configparser
+from dataclasses import dataclass
+from pathlib import Path
+
+DATASET_ROOT = Path("./evs_mot_public_dataset")
+TRAIN_PATH = DATASET_ROOT / "evs_mot-train"
+TEST_PATH  = DATASET_ROOT / "evs_mot-test"
+
+SEQUENCE_CONFIG_FILE = "seqinfo.ini"
+
+
+
+@dataclass
+class SequenceConfig:
+    folder_path: Path
+    name: str
+    imDir: str
+    frameRate: int
+    seqLength: int
+    imWidth: int
+    imHeight: int
+    imExt : str
+
+def load_sequence_config(sequence_path : Path) -> SequenceConfig:
+
+    config = configparser.ConfigParser()
+
+    config.read(sequence_path / SEQUENCE_CONFIG_FILE)
+    sequence = config["Sequence"]
+
+    return SequenceConfig(
+        folder_path=sequence_path,
+        name=sequence["name"],
+        imDir=sequence["imDir"],
+        frameRate=int(sequence["frameRate"]),
+        seqLength=int(sequence["seqLength"]),
+        imWidth=int(sequence["imWidth"]),
+        imHeight=int(sequence["imHeight"]),
+        imExt=sequence["imExt"]
+    )
+
+def load_sequences(dataset_path : Path) -> dict[str, SequenceConfig]:
+
+    sequences = {}
+    for sequence_path in dataset_path.iterdir():
+        if not sequence_path.is_dir():
+            continue
+        print(sequence_path)
+
+        sequence_config = sequence_path
+        config = load_sequence_config(sequence_path)
+
+        sequences[config.name] = config
+
+    return sequences
+
+# name : config
+
+TRAIN_SEQUENCES = load_sequences(TRAIN_PATH)
+TEST_SEQUENCES = load_sequences(TEST_PATH)
+
+print(TEST_SEQUENCES)
